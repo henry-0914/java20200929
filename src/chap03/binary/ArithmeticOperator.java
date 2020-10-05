af9db823bda8a5f17c53bfbed1e379226d9d5ff6
henry-0914 <hany7012@gmail.com>
2020-10-05 12:40:21 +0900

    overflow

---
+++ src/chap03/binary/ArithmeticOperator.java
@@ -0,0 +1,11 @@
+package chap03.binary;
+
+public class ArithmeticOperator {
+
+	public static void main(String[] args) {
+		
+		// +, -, *, / %(나머지연산)
+		
+		
+	}
+}
---
+++ src/textBook/OverflowExample.java
@@ -0,0 +1,13 @@
+package textBook;
+
+public class OverflowExample {
+	public static void main(String[] args) {
+		
+		int x = 1000000;
+		int y = 1000000;
+		int z = x * y;
+		// long 타입으로 변경 해야 됨(4byte 초과됨)
+		System.out.println(z);
+		
+	}
+}
