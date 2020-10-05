441a834a9e0e23108200187371a7545ae59041b5
henry-0914 <hany7012@gmail.com>
2020-10-05 15:37:59 +0900

    compare

---
+++ src/textBook/StringEqualsEXample.java
@@ -0,0 +1,16 @@
+package textBook;
+
+public class StringEqualsEXample {
+	public static void main(String[] args) {
+		
+		String strVar1 = "김영한";
+		String strVar2 = "김영한";
+		String strVar3 = new String("김영한");
+		
+	    System.out.println( strVar1 == strVar2);
+	    System.out.println( strVar1 == strVar3);
+	    System.out.println();
+	    System.out.println( strVar1 .equals(strVar2));
+	    System.out.println( strVar1 .equals(strVar3));
+	}
+}
