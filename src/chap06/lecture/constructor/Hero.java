e399684833bf2f6834da3ce23131248a4f38332c
henry-0914 <hany7012@gmail.com>
2020-10-12 16:53:46 +0900

    Field2

---
+++ src/chap06/lecture/constructor/Hero.java
@@ -0,0 +1,13 @@
+package chap06.lecture.constructor;
+
+public class Hero {
+
+	String name;
+	int power;
+	
+	public Hero(String heroName, int heroPower) {
+		this.name = heroName;
+		this.power = heroPower;
+		
+	}
+}
---
+++ src/chap06/textbook/s060705/Car.java
@@ -0,0 +1,28 @@
+package chap06.textbook.s060705;
+
+public class Car {
+
+		String company = "현대 자동차";
+		String model;
+		String color;
+		int maxSpeed;
+		
+		Car() {
+			
+		}
+		
+		Car(String model) {
+			this(model, "은색", 250);
+		}
+		
+		Car(String model, String color) {
+			this(model, color, 250); // 생성자
+			
+		}
+		
+		Car(String model, String color, int maxSpeed) {
+			this.model = model; // 필드
+			this.color = color;
+			this.maxSpeed = maxSpeed;
+		}
+}
