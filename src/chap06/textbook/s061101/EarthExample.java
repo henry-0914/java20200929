bc2303a8275e9faab24ecab99f2013693ca718c9
henry-0914 <hany7012@gmail.com>
2020-10-14 11:43:30 +0900

    package

--- src/chap06/textbook/s061101/EarthExample.java
+++ src/chap06/textbook/s061101/EarthExample.java
@@ -6,3 +6,4 @@ public class EarthExample {
 		System.out.println("지구의 표면적:" + Earth.EARTH_SURFACE_AREA + "km^2");
 	}
 }
+
---
+++ src/chap06/textbook/s061204/mycompany/Car.java
@@ -0,0 +1,15 @@
+package chap06.textbook.s061204.mycompany;
+
+import chap06.textbook.s061204.hankook.SnowTire;
+import chap06.textbook.s061204.hyundai.engine;
+import chap06.textbook.s061204.kumho.BigWidthTire;
+import chap06.textbook.s061204.kumho.Tire;
+
+public class Car {
+	
+	engine engine = new engine();
+	SnowTire tire1 = new SnowTire();
+	BigWidthTire tire2 = new BigWidthTire();
+	
+	Tire tire3 = new Tire();
+}
