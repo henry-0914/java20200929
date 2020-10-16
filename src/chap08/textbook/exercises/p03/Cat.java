6e94d461e10014a20d0e7dd18fe8ad7e21a892a8
henry-0914 <hany7012@gmail.com>
2020-10-16 16:32:29 +0900

    implements

---
+++ src/chap08/textbook/exercises/p03/Cat.java
@@ -0,0 +1,11 @@
+package chap08.textbook.exercises.p03;
+
+public class Cat implements Soundable {
+
+	@Override
+	public String sound() {
+		// TODO Auto-generated method stub
+		return "야옹";
+	}
+
+}
---
+++ src/chap08/textbook/exercises/p03/Dog.java
@@ -0,0 +1,11 @@
+package chap08.textbook.exercises.p03;
+
+public class Dog implements Soundable {
+
+	@Override
+	public String sound() {
+		// TODO Auto-generated method stub
+		return "멍멍";
+	}
+
+}
