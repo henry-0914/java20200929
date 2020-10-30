98f93f105fdda3e753176bc95b311c3a6c5bfc89
hany0 <hany7012@gmail.com>
2020-10-30 18:06:50 +0900

    TernaryOperator

--- src/chap03/ternary/TernaryOperator.java
+++ src/chap03/ternary/TernaryOperator.java
@@ -5,12 +5,12 @@ public class TernaryOperator {
 	public static void main(String[] args) {
 		
 		//  a ? b : c 
-		//1항 ? 2항 : 3항
-		//위 연산 결과는 b 혹은 c(1항이 True면 B, 1항이 false면 C가 나옴)
+		//1�빆 ? 2�빆 : 3�빆
+		//�쐞 �뿰�궛 寃곌낵�뒗 b �샊�� c(1�빆�씠 True硫� B, 1�빆�씠 false硫� C媛� �굹�샂)
 		boolean a = true;
 		int b = 3;
 		int c = 5;
-		
+		// 
 		System.out.println(a ?  b : c); 
 	}
 }
